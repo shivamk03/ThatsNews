d53eec39cbac9f56516f64e8addd399b4d0bb423
Shivam <you@example.com>
2023-08-15 22:18:14 +0530

    Updates

---
+++ precache-manifest.688e9ee25a0deb80457acefc30b35c2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d358d5fd43608ab0645",
+    "url": "/ThatsNews/static/css/main.77117691.chunk.css"
+  },
+  {
+    "revision": "7d358d5fd43608ab0645",
+    "url": "/ThatsNews/static/js/main.7d358d5f.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/ThatsNews/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "f7e85e7e0d5bdb7d877a",
+    "url": "/ThatsNews/static/js/2.f7e85e7e.chunk.js"
+  },
+  {
+    "revision": "3a50f91d79dbf21b7bd1",
+    "url": "/ThatsNews/static/js/runtime~main.3a50f91d.js"
+  },
+  {
+    "revision": "c6ac6c5512575e692ce3f39397cfb07b",
+    "url": "/ThatsNews/static/media/logo.c6ac6c55.png"
+  },
+  {
+    "revision": "eccd8e894ac9da062cc771a738266f21",
+    "url": "/ThatsNews/static/media/Spinner.eccd8e89.gif"
+  },
+  {
+    "revision": "13e809c1f33a0a7f29258524b8b62b58",
+    "url": "/ThatsNews/index.html"
+  }
+];
